a0041f9ae6f747db5aac017bf07934a324acd15f
maha <maha269>
2021-02-14 17:31:00 +0200

    authentication and rest api

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
